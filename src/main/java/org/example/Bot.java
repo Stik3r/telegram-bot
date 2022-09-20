4a0b709a67ea73356d6ecb93e28eb83773b12613
Mahonin Matvey <maxon.07@mail.ru>
2022-09-20 23:00:33 +0300

    Rework keyboard to inline. Bot now can memorize data

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -4,13 +4,15 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Vector;
+
 
 public class Bot extends TelegramLongPollingBot {
 
@@ -18,20 +20,19 @@ public class Bot extends TelegramLongPollingBot {
     final String BOT_TOKEN = Bot_Item.BOT_TOKEN;
     final String BOT_NAME = Bot_Item.BOT_NAME;
 
-    ReplyKeyboardMarkup replyKeyboardMarkup;
+    boolean inline = false;
 
+    Map<String, Parser> usersParsers = new HashMap<>();
+
+    InlineKeyboardMarkup inlineKeyboard = new InlineKeyboardMarkup();
 
     String[] genres;
-    Vector<ReplyKeyboardMarkup> keyBoardsGenres = new Vector<>();
-    int currentPage = 0;
-    Storage storage = new Storage();
 
     public Bot() {
         String buf = "Аниме Биографии Боевики Военные Детективы Детские Документальные Драмы Исторические" +
                 " Комедии Мелодраммы Мультфильмы Мюзиклы Приключения Семейные Спортивные Триллеры Ужасы " +
                 "Фантастика Фэнтези";
         genres = buf.split(" ");
-        initKetBoardGenres();
     }
 
     @Override
@@ -47,79 +48,71 @@ public class Bot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         try{
-            if(update.hasMessage() && update.getMessage().hasText())
+            if(update.hasMessage())
             {
-                Message inMess = update.getMessage();
-                String chatId = inMess.getChatId().toString();
-                String response = parseMessage(inMess.getText());
+                String chatId = update.getMessage().getChatId().toString();
+                String response = parseMessage(update.getMessage());
                 SendMessage outMess = new SendMessage();
 
                 outMess.setChatId(chatId);
                 outMess.setText(response);
-                outMess.setReplyMarkup(replyKeyboardMarkup);
 
+                if(inline){
+                    outMess.setReplyMarkup(inlineKeyboard);
+                    inline = false;
+                }
 
                 execute(outMess);
+            }
+            else if (update.hasCallbackQuery()){
+                String chatId = update.getCallbackQuery().getMessage().getChatId().toString();
+                SendMessage outMess = new SendMessage();
+                outMess.setChatId(chatId);
+                outMess.setText("Теперь необходимо написать год. Сообщение должно выглядить следующим образом" +
+                        "\n\"год 2017\"");
+                execute(outMess);
+
+                Parser parser = new Parser();
+                parser.genre = update.getCallbackQuery().getData();
+                parser.year = "";
+                usersParsers.put(chatId, parser);
             }
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
     }
-    public String parseMessage(String textMsg) {
+    public String parseMessage(Message msg) {
         String response;
 
-        if(textMsg.equals("/start")){
+        if(msg.getText().equals("/start")){
             response = "Приветствую, бот посылает случайные фильмы и если ты не знаешь что посмотреть то" +
                     " можешь испытать удачу, вдруг заинтересует. Для начала необходимо выбрать жанр:";
-            replyKeyboardMarkup = keyBoardsGenres.get(0);
+            CreateGenresKeyboard();
+        } else if(msg.getText().toLowerCase().contains("год") &&
+        usersParsers.containsKey(msg.getChatId().toString())){
+            usersParsers.get(msg.getChatId().toString()).year = msg.getText().substring(
+                    msg.getText().length() - 5);
+            response =  
         }
-        else if(textMsg.equals("Пред. страница")){
-            if(currentPage == 0)
-                response = "Это первая страница.";
-            else{
-                currentPage--;
-                replyKeyboardMarkup = keyBoardsGenres.get(currentPage);
-                response = "Клавиатура обнавлена \nСтраница " + (currentPage + 1);
-            }
-        }
-        else if(textMsg.equals("След. страница"))
-            if(currentPage == keyBoardsGenres.size() - 1)
-                response = "Пока что это все жанры, что у меня имеются :(";
-            else {
-                currentPage++;
-                replyKeyboardMarkup = keyBoardsGenres.get(currentPage);
-                response = "Клавиатура обнавлена \nСтраница " + (currentPage + 1);
-            }
         else
             response = "Сообщение не распознано";
 
         return response;
     }
 
-    void initKetBoardGenres(){
-        int genresCount = 0;
-        while (genresCount < genres.length){
-            ReplyKeyboardMarkup genresKeyboard = new ReplyKeyboardMarkup();
-            genresKeyboard.setResizeKeyboard(true);
-            ArrayList<KeyboardRow> keyboardRows = new ArrayList<>();
-            keyboardRows.add(CreateRow(genresCount, 3));
-            genresCount += 3;
-            keyboardRows.add(CreateRow(genresCount, 2));
-            genresCount += 2;
-            KeyboardRow pages = new KeyboardRow();
-            pages.add(new KeyboardButton("Пред. страница"));
-            pages.add(new KeyboardButton("След. страница"));
-            keyboardRows.add(pages);
-            genresKeyboard.setKeyboard(keyboardRows);
-            keyBoardsGenres.add(genresKeyboard);
+    void CreateGenresKeyboard(){
+        List<List<InlineKeyboardButton>> keyBoardRows = new Vector<>();
+        for (String genre:
+             genres) {
+            InlineKeyboardButton button = new InlineKeyboardButton();
+            button.setText(genre);
+            button.setCallbackData(genre);
+            List<InlineKeyboardButton> bufRow = new Vector<>();
+            bufRow.add(button);
+            keyBoardRows.add(bufRow);
         }
+        inlineKeyboard.setKeyboard(keyBoardRows);
+        inline = true;
     }
 
-    KeyboardRow CreateRow(int indx, int count){
-        KeyboardRow result = new KeyboardRow();
-        for(int i = 0; i < count; i++){
-            result.add(new KeyboardButton(genres[indx + i]));
-        }
-        return result;
-    }
 }
---
+++ src/main/java/org/example/Parser.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Parser {
+    public String genre;
+    public String year;
+
+
+}
